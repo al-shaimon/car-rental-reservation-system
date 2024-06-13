706e09c053767d104910ecdf391d5a76724b7d97
al-shaimon <alshaimon968@gmail.com>
2024-06-13 21:01:49 +0600

    car interface and model

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from 'mongoose';
+
+const bookingSchema = new Schema(
+  {
+    date: { type: Date, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    car: { type: Schema.Types.ObjectId, ref: 'Car', required: true },
+    startTime: { type: String, required: true },
+    endTime: { type: String },
+    totalCost: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+export const Booking = model('Booking', bookingSchema);
--- src/app/modules/car/car.model.ts
+++ src/app/modules/car/car.model.ts
@@ -1,12 +1,18 @@
 import { model, Schema } from 'mongoose';
+import { TCar } from './car.interface';
 
-const carSchema = new Schema(
+const carSchema = new Schema<TCar>(
   {
     name: { type: String, required: true },
     description: { type: String, required: true },
     color: { type: String, required: true },
     isElectric: { type: Boolean, required: true },
-    status: { type: String, default: 'available' },
+    status: {
+      type: String,
+      enum: ['available', 'unavailable'],
+      default: 'available',
+      required: true
+    },
     features: { type: [String], required: true },
     pricePerHour: { type: Number, required: true },
     isDeleted: { type: Boolean, default: false },
