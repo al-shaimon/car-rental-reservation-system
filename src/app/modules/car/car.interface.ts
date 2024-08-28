bcfe2721b6e9dc2716c90f3047dff72360b68f3d
al-shaimon <alshaimon968@gmail.com>
2024-08-29 03:29:35 +0600

    cars update

--- src/app/modules/car/car.interface.ts
+++ src/app/modules/car/car.interface.ts
@@ -3,11 +3,14 @@ import { Document } from 'mongoose';
 export interface TCar extends Document {
   name: string;
   description: string;
+  carType: string;
+  image: string;
   color: string;
   isElectric: boolean;
   features: string[];
   pricePerHour: number;
   status: 'available' | 'unavailable';
+  reviews?: string[];
   isDeleted: boolean;
   createdAt?: Date;
   updatedAt?: Date;
--- src/app/modules/car/car.validation.ts
+++ src/app/modules/car/car.validation.ts
@@ -3,22 +3,28 @@ import { z } from 'zod';
 const createCarValidationSchema = z.object({
   name: z.string(),
   description: z.string(),
+  carType: z.string(),
+  image: z.string(),
   color: z.string(),
   isElectric: z.boolean(),
   features: z.array(z.string()).nonempty(),
   pricePerHour: z.number().positive(),
   status: z.enum(['available', 'unavailable']).default('available'),
+  reviews: z.array(z.string()).optional(),
   isDeleted: z.boolean().default(false),
 });
 
 const updateCarValidationSchema = z.object({
   name: z.string().optional(),
   description: z.string().optional(),
+  carType: z.string().optional(),
+  image: z.string().optional(),
   color: z.string().optional(),
   isElectric: z.boolean().optional(),
   features: z.array(z.string()).nonempty().optional(),
   pricePerHour: z.number().positive().optional(),
   status: z.enum(['available', 'unavailable']).default('available').optional(),
+  reviews: z.array(z.string()).optional(),
   isDeleted: z.boolean().default(false).optional(),
 });
 
